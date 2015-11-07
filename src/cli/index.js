ca572870762425d709d5d36f0461cb16de2bf650
gocreating <gocreating@gmail.com>
2015-11-07 18:12:56 +0800

    use commander as cli tool

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,37 +1,38 @@
 #! /usr/bin/env node
 
 import {exec} from 'child_process';
+import program from 'commander';
+import pkg from '../package.json';
 
-export function displayUsage() {
-  console.log(`
-    Usage: sd <command> [options]
+program
+  .version(pkg.version);
 
-    Commands:
-      help    display help message
-      run     launch exseed app
-  `);
-}
-
-export function run() {
-  const userArgs = process.argv.slice(2);
-  const command = userArgs[0];
-
-  // no command
-  if (command === undefined) {
-    displayUsage();
-  }
-  // help
-  else if (command === 'help') {
-    displayUsage();
-  }
-  // run
-  else if (command === 'run') {
+program
+  .command('serve')
+  .alias('s')
+  .description('launch exseed app')
+  .option(
+    '-e, --env <env>',
+    'specify environment (development|test|production)',
+    'development')
+  .option(
+    '-w, --watch <watch>',
+    'watching the changes of files',
+    false)
+  .action((options) => {
     const cmd = 'node ./build/debug/app.js';
-    console.log(`execute ${cmd}\n`);
-    let nodeApp = exec(cmd);
+    console.log(`execute ${cmd} under ${options.env} environment`);
+    let nodeApp = exec(cmd, {
+      env: {
+        NODE_ENV: options.env,
+      },
+    });
     nodeApp.stdout.pipe(process.stdout);
     nodeApp.stderr.pipe(process.stdout);
-  }
-}
+  });
 
-run();
+program.parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
+}
