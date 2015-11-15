8ee30c62179b57e3d4bb3807e3b6a09380780f0a
gocreating <gocreating@gmail.com>
2015-11-15 19:37:35 +0800

    Remove old routing rules

--- example/src/basic/index.js
+++ example/src/basic/index.js
@@ -16,29 +16,6 @@ class BasicApp extends App {
    * @param {object} models - all models of the project
    */
   routing(app, router, models) {
-    app.get('/', (req, res) => {
-      res.send('home');
-    });
-
-    app.get('/about', (req, res) => {
-      res.send('about');
-    });
-
-    app.get('/react', (req, res) => {
-      res.send(`
-        <!doctype html>
-        <html>
-          <head>
-            <title>React Transform Boilerplate</title>
-          </head>
-          <body>
-            <div id='root'>
-            </div>
-            <script src="/static/bundle.js"></script>
-          </body>
-        </html>
-      `);
-    });
   }
 
   onError(err, req, res) {
