652c4b9d0b68fb30f6b8c2d593bdde076ce95e3e
gocreating <gocreating@gmail.com>
2015-11-19 01:30:19 +0800

    Change directory name
    Change `basic` to `exseed.contrib.basic` and `user` to
    `exseed.contrib.user`

--- example/src/app.js
+++ example/src/app.js
@@ -2,8 +2,8 @@ import 'source-map-support/register';
 import * as exseed from 'exseed';
 import settings from './settings.server';
 
-exseed.registerApp('basic', './basic');
-exseed.registerApp('user', './user');
+exseed.registerApp('basic', './exseed.contrib.basic');
+exseed.registerApp('user', './exseed.contrib.user');
 
 exseed.run(settings, (err, models, port) => {
   if (err) {
