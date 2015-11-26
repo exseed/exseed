bfb13b28041c21898b804c63ce19b0ffd87ff7bf
gocreating <gocreating@gmail.com>
2015-11-27 01:43:48 +0800

    Add remind comment

--- example/src/exseed.contrib.basic/routes.js
+++ example/src/exseed.contrib.basic/routes.js
@@ -6,6 +6,10 @@ import HomePage from './flux/views/pages/HomePage';
 import AboutPage from './flux/views/pages/AboutPage';
 
 export default class App extends React.Component {
+  // don't use the spread operator to pass props
+  // like this way: `<Location {...this.props} />`
+  // it will cause `react-router-component` behaves weird
+  // and is hard to debug
   render() {
     return <CaptureClicks>
       <Locations path={this.props.path}>
