e06fb44ea7509a676bd6c2dadb4f08c9c231f63a
gocreating <gocreating@gmail.com>
2016-01-14 01:08:16 +0800

    Add PageNotFound error

---
+++ src/classes/PageNotFound.js
@@ -0,0 +1,11 @@
+import { Err } from '.';
+
+export default class PageNotFound extends Err {
+  constructor(props) {
+    super(props, {
+      title: 'Page not found',
+      message: 'The url you are requesting does not exist',
+      status: 404,
+    });
+  }
+};
--- src/classes/index.js
+++ src/classes/index.js
@@ -1,3 +1,4 @@
 export const App = require('./App').default;
 export const Err = require('./Err').default;
+export const PageNotFound = require('./PageNotFound').default;
 export default module.exports;
